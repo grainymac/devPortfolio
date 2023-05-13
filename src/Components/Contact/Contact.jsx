e89de4b8abf6eb9a533e7830f56e86302cc4feac
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-05-12 19:29:14 -0500

    Added some structure to contact form but need to install packages

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import './Contact.ssss'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const contactForm = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_smkz3xo','template_j34068y', form.current, '5zEiwTNMl_2Xx4cpQ')
+            .then(() => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+              },() => {
+              alert('Failed to send the message, please try again!')
+              }
+            )
+  }
+
   return (
-    <div>Contact</div>
+    <>
+      <div className='contact-page'>
+        <p className='contact-intro-text'>
+
+        </p>
+        <div className='contact-form'>
+          <form ref={contactForm} onSubmit={ sendEmail } ></form>
+        </div>
+      </div>
+    </>
   )
 }
 
