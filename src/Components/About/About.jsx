3a4098fb440fc7494d3f5eb827be89362a0edf35
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-05-22 00:35:36 -0500

    fixed about me

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import './About.sass'
 import Profile from './images/profile.jpg'
 
 const About = () => {
   return (
     <>
       <div className='about-page' id='about'>
+        <h2>About</h2>
         <div className='about-container'>
-          {/* <img className='profile-photo' src={Profile} alt='profile photo' /> */}
+          <img className='profile-photo' src={Profile} alt='profile photo' />
           <div className='about-text-container'>
-            <h2>About</h2>
             <span className='line'></span>
             <p>
               Hey, whats up?! I'm a front-end developer with a passion for creating beautiful and functional user interfaces using React.js and SCSS. Also as a creative with diverse skills and interests, I have the ability to explore and express my ideas through a variety of mediums, each informing and enhancing the others. With my background in graphic design, I bring a unique perspective to my work and enjoy exploring new ways to integrate design into my coding projects. I'm a highly creative person who is always looking for ways to think outside the box and push my boundaries. My dream is to work on a team that develops solutions and applications that have a positive impact on people's lives. I recently graduated from a coding bootcamp called Turing School of Software & Design and am excited to continue learning and growing in my career as a developer.
