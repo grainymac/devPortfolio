b136f6693d8bfae3dd0eff3ab3af752bbf476873
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-05-23 07:07:04 -0500

    changes

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Work.sass'
+import WorkImageSlider from '../../Containers/WorkImageSlider/WorkImageSlider'
+import discoverIt from './assets/discoverIt.png'
+import dokes from './assets/dokes.png'
+import theNeoteric from './assets/theNeoteric.png'
+import photoPortfolio from './assets/photographyPortfolio.png'
+import theBored from './assets/theBored.png'
 
 const Work = () => {
+  const slides = [
+    {url: `${discoverIt}`, title: 'DiscoverIt'},
+    {url: `${photoPortfolio}`, title: 'Photography Portfolio'},
+    {url: `${theNeoteric}`, title: 'The Neoteric'},
+    {url: `${dokes}`, title: 'Dokes'},
+    {url: `${theBored}`, title: 'The Bored'}
+  ]
+
   return (
-    <div>Work</div>
+    <>
+      <div className='work-container' id='work'>
+      <h2 className='work-title'>Work</h2>
+        <WorkImageSlider slides={slides} />
+      </div>
+    </>
   )
 }
 
