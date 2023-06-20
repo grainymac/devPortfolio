caa32b86842d43dee6df5bd7125d5cbb47a04d7e
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-06-20 16:39:26 -0500

    error handling

--- src/Components/ErrorHandling/BadURL.jsx
+++ src/Components/ErrorHandling/BadURL.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const BadURL = () => {
   return (
-    <div className='badURL-container'>
+    <div className='badURL-container' id='blog'>
         <h2 className='badUrl-title'>You are on the wrong page!<p>Please click the button below to go back home.</p></h2>
         <Link to='/'><button className='homeButton'>HOME</button></Link>
     </div>
