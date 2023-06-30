e463ce0c4cba223cd5ddc48092979c425467e7c4
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-06-29 23:10:50 -0500

    changed about me

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,8 +14,11 @@ const About = () => {
           <div className='about-text-container'>
             <span className='line'></span>
             <p>
-            I'm a front-end developer obsessed with creating beautiful, functional user interfaces with React.js and SCSS. I'm a creative individual with a wide range of skills and interests, so I'm able to express myself through several mediums, each of which informs and enhances the others. My background in graphic design gives me a unique perspective on my work, and I love finding innovative ways to merge design with coding. Being creative keeps me on my toes, pushing my capabilities and thinking outside the box. Ultimately, I want to be part of a team that develops solutions and applications that make a positive impact on people. Recently, I graduated from the Turing School of Software & Design, a coding bootcamp where I learned new skills online. Due to its strong community-based learning approach, I found the experience immensely rewarding, despite the fast-paced environment. At Turing, I focused on building client-side applications using JavaScript, React and other libraries. I've been keeping myself busy by developing personal apps and websites since graduating to find a job in the tech field. My ambition and commitment to continuous learning will drive me forward in my career as a software developer despite this hurdle.
+            I'm a front-end developer obsessed with creating beautiful, functional user interfaces with React.js and SCSS. I'm a creative individual with a wide range of skills and interests, so I'm able to express myself through several mediums, each of which informs and enhances the others. My background in graphic design gives me a unique perspective on my work, and I love finding innovative ways to merge design with coding. Being creative keeps me on my toes, pushing my capabilities and thinking outside the box. Ultimately, I want to be part of a team that develops solutions and applications that make a positive impact on people. My ambition and commitment to continuous learning will drive me forward in my career as a software developer despite this hurdle.
             </p>
+            <a href='https://resume.creddle.io/resume/5l3w33n9sze'>
+              <button className='about-button'>RESUME</button>
+            </a>
           </div>
         </div>
       </div>
